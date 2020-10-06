c7085d501f26280cc7d0407c2dd0a3ca3f1194ed
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-06 13:59:45 +0300

    Add service for user and the concrate class

---
+++ JWT.API/API/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+    }
+}
---
+++ JWT.API/API/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using API.Models;
+using API.Settings;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly JwtSettings _jwt;
+
+        public UserService(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IOptions<JwtSettings> jwt)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _jwt = jwt.Value;
+        }
+
+        public async Task Test()
+        {
+        }
+    }
+}
