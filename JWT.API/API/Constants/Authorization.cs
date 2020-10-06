34b77dcec2bb767b7b06a6b5430e94884ddbe603
dimitkos <User1@DESKTOP-D6C1HHM>
2020-10-06 13:19:19 +0300

    Add method to seed data

---
+++ JWT.API/API/Constants/Authorization.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Constants
+{
+    public class Authorization
+    {
+        public enum Roles
+        {
+            Administrator,
+            Moderator,
+            User
+        }
+        public const string defaultUsername = "user";
+        public const string defaultEmail = "user@secureapi.com";
+        public const string defaultPassword = "Pa$$w0rd.";
+        public const Roles defaultRole = Roles.User;
+    }
+}
---
+++ JWT.API/API/Contexts/ApplicationDbContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using API.Constants;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Contexts
+{
+    public class ApplicationDbContextSeed
+    {
+        public static async Task SeedEssentialsAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Authorization.Roles.Administrator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Authorization.Roles.Moderator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Authorization.Roles.User.ToString()));
+
+            //Seed Default User
+            var defaultUser = new ApplicationUser 
+            { 
+                UserName = Authorization.defaultUsername, 
+                Email = Authorization.defaultEmail, 
+                EmailConfirmed = true, 
+                PhoneNumberConfirmed = true
+            };
+            
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                await userManager.CreateAsync(defaultUser, Authorization.defaultPassword);
+                await userManager.AddToRoleAsync(defaultUser, Authorization.defaultRole.ToString());
+            }
+        }
+    }
+}
